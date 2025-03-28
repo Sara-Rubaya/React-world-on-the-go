7f3811b63225954db94177105f9be3e4fa038597
Sara <sara@Mac.lan>
2025-03-28 23:39:31 +0600

    added more new react

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,20 @@
+import React, { use } from 'react';
+import Country from '../Country/Country';
+import './countries.css'
+
+const Countries = ({countriesPromise}) => {
+    const countries = use(countriesPromise);
+    console.log(countries);
+    return (
+        <div>
+            <h1>Travelling Countries: {countries.length}</h1>
+            <div className='countries'>
+            {
+                countries.map(country => <Country key={country.cca3} country={country}></Country>)
+            }
+            </div>
+        </div>
+    );
+};
+
+export default Countries;
